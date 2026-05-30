d5142a0d520cc19374069cea7b3acd7f9a404348
user <keithbogi@gmail.com>
2026-05-30 14:42:29 +0800

    CCC

---
+++ DigiERP/UserControl/Auth/FrmAuth.cs
@@ -0,0 +1,17 @@
+﻿using DigiERP.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiERP.UserControl.Auth
+{
+    public partial class FrmAuth : CommonForm
+    {
+        public FrmAuth()
+        {
+            InitializeComponent();
+        }   
+    }
+}
